d961a3f7ce2b9a46df10874e091ffa48b9c42568
RollingStoned-22 <132134205+RollingStoned-22@users.noreply.github.com>
2024-09-14 20:34:54 +0530

    ver 1.2

--- src/components/DestinationSearch/index.js
+++ src/components/DestinationSearch/index.js
@@ -1,27 +1,56 @@
 // Write your code here
+import {Component} from 'react'
 import './index.css'
-import DestinationItem from '../DestinationItem'
+import DestinationItem from '../DestinationItem/index'
 
-const DestinationSearch = props => {
-  const {destinationsList} = props
-  state = {searchInput: '', destinationsList: destinationsList}
-  console.log(state)
-  return (
-    <div className="container">
-      <h1>Destination Search</h1>
-      <input type="search" placeholder="Search" />
-      <div className="destinations-container">
-        <div>
-          {destinationsList.map(destinationDetails => (
-            <DestinationItem
-              destinationDetails={destinationDetails}
-              key={destinationDetails.id}
-            />
-          ))}
+class DestinationSearch extends Component {
+  state = {searchInput: '', destinations: this.props.destinationsList}
+  changeCase = () => {
+    const {destinations} = this.state
+    const allDestinations = destinations.map(place => {
+      place.name.toLowerCase()
+    })
+    this.setState({destinations: allDestinations})
+  }
+
+  onChangeInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  deleteDestination = id => {
+    const {destinations} = this.state
+    const filteredDestinations = destinations.filter(place => place.id !== id)
+    this.setState({destinations: filteredDestinations})
+  }
+
+  render() {
+    const {destinations, searchInput} = this.state
+    const filteredDestinations = destinations.filter(place =>
+      place.name.includes(searchInput),
+    )
+    console.log(destinations)
+    return (
+      <div className="container">
+        <h1>Destination Search</h1>
+        <input
+          type="search"
+          placeholder="Search"
+          onChange={this.onChangeInput}
+        />
+        <div className="destinations-container">
+          <ul>
+            {filteredDestinations.map(destinationDetails => (
+              <DestinationItem
+                destinationDetails={destinationDetails}
+                deleteDestination={this.deleteDestination}
+                key={destinationDetails.id}
+              />
+            ))}
+          </ul>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default DestinationSearch
